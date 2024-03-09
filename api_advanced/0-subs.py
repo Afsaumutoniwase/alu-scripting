a18c71020acb09192240e16097e514dc1de9bd69
Afsaumutoniwase <a.umutoniwa@alustudent.com>
2024-03-09 11:34:57 -0800

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,33 +2,17 @@
 """
 0-subs.py
 """
-
 import requests
-
 def number_of_subscribers(subreddit):
-    """
-    Retrieve the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-
-    Returns:
-        int: The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
-    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyBot/0.1"}  # Setting the user agent
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code != 200:
-        return 0
-    else:
-        return response.json().get("data").get("subscribers")
-
-def main():
-    subreddit = input("Enter the name of the subreddit: ")
-    subscribers = number_of_subscribers(subreddit)
-    print(f"The number of subscribers in r/{subreddit} is {subscribers}")
-
-if __name__ == "__main__":
-    main()
-
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+    return 0
+# Test the function
+subreddit = 'learnpython'
+subscribers = number_of_subscribers(subreddit)
+print(f"The number of subscribers in r/{subreddit} is: {subscribers}")
