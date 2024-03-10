fd20fae75ebfe946a171242675f5cab1c7482b4a
Afsaumutoniwa <afsaumutoniwa@pontemanalytics.com>
2024-03-10 03:25:21 +0200

    api

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,12 +2,15 @@
 """0-subs.py"""
 
 import requests
+
+
 def number_of_subscribers(subreddit):
-    headers = {'User-Agent': 'Mozilla/5.0'}  # Setting a custom User-Agent
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Reddit subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-    return 0
+    if response.status_code != 200:
+        return 0
+    else:
+        return response.json().get("data").get("subscribers")
