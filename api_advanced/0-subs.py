3e9d4dc093f49b08d27b1ddc4e0b3ce1b98870da
Afsa Umutoniwase <a.umutoniwa@alustudent.com>
2024-03-31 16:27:51 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
-"""0-subs.py"""
-
+'''
+    This module contains the function number_of_subscribers
+'''
 import requests
-
+from sys import argv
 
 def number_of_subscribers(subreddit):
-    """Reddit subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    '''
+        Returns the number of subscribers for a given subreddit
+    '''
+    user_agent = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    
+    try:
+        response = requests.get(url, headers=user_agent)
+        response.raise_for_status()  # Raise an error for bad status codes
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data:", e)
         return 0
-    else:
-        return response.json().get("data").get("subscribers")
+    except KeyError:
+        print("Subreddit not found or unable to fetch data.")
+        return 0
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python3 script_name.py subreddit_name")
+        exit(1)
+    subreddit_name = argv[1]
+    subscriber_count = number_of_subscribers(subreddit_name)
+    print(subscriber_count)
