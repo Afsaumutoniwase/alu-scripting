ecae77e8a5c2a7776826e7b70ad75bd50eb6e2f3
Afsa Umutoniwase <a.umutoniwa@alustudent.com>
2024-03-31 16:36:38 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,8 +3,10 @@
     This module contains the function number_of_subscribers
 '''
 
+
 import requests
 from sys import argv
+
 
 def number_of_subscribers(subreddit):
     '''
@@ -26,10 +28,12 @@ def number_of_subscribers(subreddit):
         print("Subreddit not found or unable to fetch data.")
         return 0
 
+
 if __name__ == "__main__":
     if len(argv) != 2:
         print("Usage: python3 script_name.py subreddit_name")
         exit(1)
+    
     subreddit_name = argv[1]
     subscriber_count = number_of_subscribers(subreddit_name)
     print(subscriber_count)
