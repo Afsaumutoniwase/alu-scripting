a54cbe8abd35287e18912969f83c4e18884390d5
Afsa Umutoniwase <a.umutoniwa@alustudent.com>
2024-03-31 16:30:17 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-"""1-top_ten.py"""
-
+'''
+    This module contains the function top_ten
+'''
 import requests
-
+from sys import argv
 
 def top_ten(subreddit):
-    """Top ten subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'My-User-Agent'}
+    '''
+        Returns the top ten posts for a given subreddit
+    '''
+    user_agent = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=user_agent)
+        response.raise_for_status()  # Raise an error for bad status codes
         data = response.json()
-        posts = data.get('data').get('children')
-        [print(post.get('data').get('title')) for post in posts]
-    else:
-        print(None)
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data:", e)
+    except KeyError:
+        print("Subreddit not found or unable to fetch data.")
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python3 script_name.py subreddit_name")
+        exit(1)
+    subreddit_name = argv[1]
+    top_ten(subreddit_name)
